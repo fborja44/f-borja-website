20fb0cfc609ddfc27843f537131a50c460d83b88
Francis Borja <fborja4400@gmail.com>
2021-06-25 18:37:53 -0400

    feat: links, page templates, and favicon

--- routes/user.js
+++ routes/user.js
@@ -15,4 +15,48 @@ router.get('/', async (req, res) => {
     }
 });
 
+/**
+ * Renders the about page
+ */
+ router.get('/about', async (req, res) => {
+    try {
+        res.render('pages/about', { title: "About" });
+    } catch (e) {
+        res.status(500).json({ status: 500, error: 'Something went wrong with the server.'});
+    }
+});
+
+/**
+ * Renders the projects page
+ */
+ router.get('/projects', async (req, res) => {
+    try {
+        res.render('pages/projects', { title: "Projects" });
+    } catch (e) {
+        res.status(500).json({ status: 500, error: 'Something went wrong with the server.'});
+    }
+});
+
+/**
+ * Renders the contact page
+ */
+ router.get('/contact', async (req, res) => {
+    try {
+        res.render('pages/contact', { title: "Contact" });
+    } catch (e) {
+        res.status(500).json({ status: 500, error: 'Something went wrong with the server.'});
+    }
+});
+
+/**
+ * Renders the blog page
+ */
+ router.get('/blog', async (req, res) => {
+    try {
+        res.render('pages/blog', { title: "Blog" });
+    } catch (e) {
+        res.status(500).json({ status: 500, error: 'Something went wrong with the server.'});
+    }
+});
+
 module.exports = router;
